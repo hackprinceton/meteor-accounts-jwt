0d60f8fe89e563bdd38f035d0dac748aa290195e
Casey Chow <casey.chow@gmail.com>
2017-12-18 19:48:55 +0000

    remove IIFEs

--- jwt_login_server.js
+++ jwt_login_server.js
@@ -1,115 +1,113 @@
 /* global JWTLogin: true */
 JWTLogin = {};
 
-(function () {
-  const jwt = Npm.require('jsonwebtoken');
+const jwt = Npm.require('jsonwebtoken');
 
-  // Can override this later to change secret
-  JWTLogin.secret = (Meteor.settings.JWTLogin &&
-                      Meteor.settings.JWTLogin.secret);
+// Can override this later to change secret
+JWTLogin.secret = (Meteor.settings.JWTLogin &&
+                    Meteor.settings.JWTLogin.secret);
 
-  // Options for getToken
-  JWTLogin.tokenOptions = (Meteor.settings.JWTLogin &&
-                            Meteor.settings.JWTLogin.tokenOptions);
-  JWTLogin.tokenOptions = JWTLogin.tokenOptions || {
-    expiresIn: 48 * 60,
-  };
+// Options for getToken
+JWTLogin.tokenOptions = (Meteor.settings.JWTLogin &&
+                          Meteor.settings.JWTLogin.tokenOptions);
+JWTLogin.tokenOptions = JWTLogin.tokenOptions || {
+  expiresIn: 48 * 60,
+};
 
-  // Options for verifyToken
-  JWTLogin.verifyOptions = (Meteor.settings.JWTLogin &&
-                             Meteor.settings.JWTLogin.verifyOptions);
-  JWTLogin.verifyOptions = JWTLogin.verifyOptions || {
-    ignoreExpiration: false,
-  };
+// Options for verifyToken
+JWTLogin.verifyOptions = (Meteor.settings.JWTLogin &&
+                            Meteor.settings.JWTLogin.verifyOptions);
+JWTLogin.verifyOptions = JWTLogin.verifyOptions || {
+  ignoreExpiration: false,
+};
 
-  // Get token to verify e-mail address
-  JWTLogin.getToken = function (email) {
-    check(email, String);
-    check(this.secret, String);
-    return jwt.sign({ email }, this.secret, this.tokenOptions);
-  };
+// Get token to verify e-mail address
+JWTLogin.getToken = function (email) {
+  check(email, String);
+  check(this.secret, String);
+  return jwt.sign({ email }, this.secret, this.tokenOptions);
+};
 
-  // Verify a token
-  JWTLogin.verifyToken = function (token) {
-    check(token, String);
-    check(this.secret, String);
-    return jwt.verify(token, this.secret, this.verifyOptions);
-  };
+// Verify a token
+JWTLogin.verifyToken = function (token) {
+  check(token, String);
+  check(this.secret, String);
+  return jwt.verify(token, this.secret, this.verifyOptions);
+};
 
-  // Register a login handler
-  Accounts.registerLoginHandler('jwt_login', (options) => {
-    if (!_.isString(options.jwt)) {
-      return undefined; // Don't handle
+// Register a login handler
+Accounts.registerLoginHandler('jwt_login', (options) => {
+  if (!_.isString(options.jwt)) {
+    return undefined; // Don't handle
+  }
+
+  // Payload should be of form {email: email}
+  let email;
+  try {
+    email = JWTLogin.verifyToken(options.jwt).email;
+  } catch (err) {
+    if (err.name === 'JsonWebTokenError') {
+      return { error: new Meteor.Error(400, 'invalid-token') };
+    } else if (err.name === 'TokenExpiredError') {
+      return { error: new Meteor.Error(400, 'expired-token') };
     }
 
-    // Payload should be of form {email: email}
+    throw err; // Unknown error
+  }
+
+  if (!_.isString(email)) {
+    return { error: new Meteor.Error(400, 'invalid-token') };
+  }
+
+  // Find the actual user object
+  const user = Meteor.users.findOne({
+    'emails.address': email,
+  });
+
+  // Existing user => verify user if necessary
+  if (user) {
+    const emailObj = _.findWhere(user.emails, { address: email });
+    if (!emailObj.verified) {
+      Meteor.users.update({
+        _id: user._id,
+        'emails.address': email,
+      }, {
+        $set: {
+          'emails.$.verified': true,
+        },
+      });
+    }
+
+    // Return login result for this user
+    return {
+      userId: user._id,
+    };
+  }
+
+  // Else no user. Throw error so client knows to init user creation
+  return {
+    error: new Meteor.Error(404, 'user-not-found'),
+  };
+});
+
+// Override insertUserDoc to mark e-mails as verified on creation when
+// the 'jwt' option is set
+const oldInsertUserDoc = Accounts.insertUserDoc;
+Accounts.insertUserDoc = function (options, user) {
+  if (_.isString(options.jwt)) {
     let email;
     try {
       email = JWTLogin.verifyToken(options.jwt).email;
     } catch (err) {
-      if (err.name === 'JsonWebTokenError') {
-        return { error: new Meteor.Error(400, 'invalid-token') };
-      } else if (err.name === 'TokenExpiredError') {
-        return { error: new Meteor.Error(400, 'expired-token') };
-      }
-
-      throw err; // Unknown error
+      // Invalid token -> ignore
     }
-
-    if (!_.isString(email)) {
-      return { error: new Meteor.Error(400, 'invalid-token') };
+    if (email) {
+      // Replace emails var with verified address
+      user.emails = [{
+        address: email,
+        verified: true,
+      }];
     }
-
-    // Find the actual user object
-    const user = Meteor.users.findOne({
-      'emails.address': email,
-    });
-
-    // Existing user => verify user if necessary
-    if (user) {
-      const emailObj = _.findWhere(user.emails, { address: email });
-      if (!emailObj.verified) {
-        Meteor.users.update({
-          _id: user._id,
-          'emails.address': email,
-        }, {
-          $set: {
-            'emails.$.verified': true,
-          },
-        });
-      }
-
-      // Return login result for this user
-      return {
-        userId: user._id,
-      };
-    }
-
-    // Else no user. Throw error so client knows to init user creation
-    return {
-      error: new Meteor.Error(404, 'user-not-found'),
-    };
-  });
-
-  // Override insertUserDoc to mark e-mails as verified on creation when
-  // the 'jwt' option is set
-  const oldInsertUserDoc = Accounts.insertUserDoc;
-  Accounts.insertUserDoc = function (options, user) {
-    if (_.isString(options.jwt)) {
-      let email;
-      try {
-        email = JWTLogin.verifyToken(options.jwt).email;
-      } catch (err) {
-        // Invalid token -> ignore
-      }
-      if (email) {
-        // Replace emails var with verified address
-        user.emails = [{
-          address: email,
-          verified: true,
-        }];
-      }
-    }
-    return oldInsertUserDoc.apply(Accounts, arguments);
-  };
-}());
+  }
+  return oldInsertUserDoc.apply(Accounts, arguments);
+};
--- jwt_login_tests.js
+++ jwt_login_tests.js
@@ -1,128 +1,126 @@
-(function () {
-  // Auto-set something for our secret
-  if (Meteor.isServer) {
-    JWTLogin.secret = Random.secret();
-  }
+// Auto-set something for our secret
+if (Meteor.isServer) {
+  JWTLogin.secret = Random.secret();
+}
 
-  // Meteor methods for testing server code
-  if (Meteor.isServer) {
-    Meteor.methods({
-      // Get a token for a given email
-      'jwtlogin/getToken': function (email) {
-        return { token: JWTLogin.getToken(email) };
-      },
+// Meteor methods for testing server code
+if (Meteor.isServer) {
+  Meteor.methods({
+    // Get a token for a given email
+    'jwtlogin/getToken': function (email) {
+      return { token: JWTLogin.getToken(email) };
+    },
 
-      // Get a token for a new user altogether
-      'jwtlogin/getTokenForNewUser': function () {
-        const email = `e${Random.id(17)}@example.com`;
-        const userId = Accounts.createUser({ email, password: 'pass' });
-        return {
-          userId,
-          email,
-          token: JWTLogin.getToken(email),
-        };
-      },
-    });
-  }
+    // Get a token for a new user altogether
+    'jwtlogin/getTokenForNewUser': function () {
+      const email = `e${Random.id(17)}@example.com`;
+      const userId = Accounts.createUser({ email, password: 'pass' });
+      return {
+        userId,
+        email,
+        token: JWTLogin.getToken(email),
+      };
+    },
+  });
+}
 
-  // All tests start with client
-  if (Meteor.isClient) {
-    Tinytest.addAsync(
-      'Should be able to login to existing user with token',
-      (test, done) => {
-        Meteor.call('jwtlogin/getTokenForNewUser', (err, res) => {
-          if (err) { throw err; }
-          if (res) {
-            Accounts.loginWithJWT(res.token, (err) => {
+// All tests start with client
+if (Meteor.isClient) {
+  Tinytest.addAsync(
+    'Should be able to login to existing user with token',
+    (test, done) => {
+      Meteor.call('jwtlogin/getTokenForNewUser', (err, res) => {
+        if (err) { throw err; }
+        if (res) {
+          Accounts.loginWithJWT(res.token, (err) => {
+            if (err) { throw err; }
+
+            // No error -> make sure we're logged in
+            test.equal(Meteor.userId(), res.userId);
+
+            // Make sure email is set currently
+            const user = Meteor.user();
+            test.equal(user.emails[0].address, res.email);
+            test.equal(user.emails[0].verified, true);
+            done();
+          });
+        }
+      });
+    },
+  );
+
+  Tinytest.addAsync(
+    'Login for non-existent user should throw error',
+    (test, done) => {
+      const email = `e${Random.id(17)}@example.com`;
+      Meteor.call('jwtlogin/getToken', email, (err, res) => {
+        if (err) { throw err; }
+        if (res) {
+          Accounts.loginWithJWT(res.token, (err) => {
+            test.isTrue(err);
+            test.equal(err.error, 404);
+            test.equal(err.reason, 'user-not-found');
+            done();
+          });
+        }
+      });
+    },
+  );
+
+  Tinytest.addAsync(
+    'Creating user with jwt option mark email as verified',
+    (test, done) => {
+      const email = `e${Random.id(17)}@example.com`;
+      Meteor.call('jwtlogin/getToken', email, (err, res) => {
+        if (err) { throw err; }
+        if (res) {
+          // NB: The email in the JWT should override the e-mail given
+          Accounts.createUser(
+            {
+              email: 'whatever@example.com',
+              password: 'pass',
+              jwt: res.token,
+            },
+            (err) => {
               if (err) { throw err; }
 
-              // No error -> make sure we're logged in
-              test.equal(Meteor.userId(), res.userId);
-
-              // Make sure email is set currently
+              // Make sure email is verified
               const user = Meteor.user();
-              test.equal(user.emails[0].address, res.email);
+              test.equal(user.emails[0].address, email);
               test.equal(user.emails[0].verified, true);
               done();
-            });
-          }
-        });
-      },
-    );
+            },
+          );
+        }
+      });
+    },
+  );
 
-    Tinytest.addAsync(
-      'Login for non-existent user should throw error',
-      (test, done) => {
-        const email = `e${Random.id(17)}@example.com`;
-        Meteor.call('jwtlogin/getToken', email, (err, res) => {
-          if (err) { throw err; }
-          if (res) {
-            Accounts.loginWithJWT(res.token, (err) => {
-              test.isTrue(err);
-              test.equal(err.error, 404);
-              test.equal(err.reason, 'user-not-found');
+  Tinytest.addAsync(
+    'Creating user with inalid jwt option should not verify',
+    (test, done) => {
+      const email = `e${Random.id(17)}@example.com`;
+      Meteor.call('jwtlogin/getToken', email, (err, res) => {
+        if (err) { throw err; }
+        if (res) {
+          Accounts.createUser(
+            {
+              email,
+              password: 'pass',
+              jwt: 'gibberish',
+            },
+            (err) => {
+              if (err) { throw err; }
+
+              // Make sure email is not verified
+              const user = Meteor.user();
+              test.equal(user.emails[0].address, email);
+              test.equal(user.emails[0].verified, false);
               done();
-            });
-          }
-        });
-      },
-    );
-
-    Tinytest.addAsync(
-      'Creating user with jwt option mark email as verified',
-      (test, done) => {
-        const email = `e${Random.id(17)}@example.com`;
-        Meteor.call('jwtlogin/getToken', email, (err, res) => {
-          if (err) { throw err; }
-          if (res) {
-            // NB: The email in the JWT should override the e-mail given
-            Accounts.createUser(
-              {
-                email: 'whatever@example.com',
-                password: 'pass',
-                jwt: res.token,
-              },
-              (err) => {
-                if (err) { throw err; }
-
-                // Make sure email is verified
-                const user = Meteor.user();
-                test.equal(user.emails[0].address, email);
-                test.equal(user.emails[0].verified, true);
-                done();
-              },
-            );
-          }
-        });
-      },
-    );
-
-    Tinytest.addAsync(
-      'Creating user with inalid jwt option should not verify',
-      (test, done) => {
-        const email = `e${Random.id(17)}@example.com`;
-        Meteor.call('jwtlogin/getToken', email, (err, res) => {
-          if (err) { throw err; }
-          if (res) {
-            Accounts.createUser(
-              {
-                email,
-                password: 'pass',
-                jwt: 'gibberish',
-              },
-              (err) => {
-                if (err) { throw err; }
-
-                // Make sure email is not verified
-                const user = Meteor.user();
-                test.equal(user.emails[0].address, email);
-                test.equal(user.emails[0].verified, false);
-                done();
-              },
-            );
-          }
-        });
-      },
-    );
-  }
-}());
+            },
+          );
+        }
+      });
+    },
+  );
+}
